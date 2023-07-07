a92bc50f2292d4cb0a2b87a0f7a2b1d39cdcc177
Joao Marchete <elisamarchete@gmail.com>
2023-07-07 14:21:47 -0400

    updated server file

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,13 +1,16 @@
 const router = require("express").Router();
 const {
-    getAllUsers,
-    getUserById,
-    createUser,
-    updateUser,
-    deleteUser,
+  getAllUsers,
+  getUserById,
+  createUser,
+  updateUser,
+  deleteUser,
 } = require("../../controllers/user-controller");
 
+// http://localhost:3001/api/users
 router.route("/").get(getAllUsers).post(createUser);
-router.route("/:userId").get(getUserById).put(updateUser).delete(deleteUser)
+
+// http://localhost:3001/api/users/:userId
+router.route("/:userId").get(getUserById).put(updateUser).delete(deleteUser);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,70 +1,13 @@
 const express = require("express");
 const db = require("./config/connection");
-const { User, Thought } = require("./models");
+const routes = require("./routes");
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
-// Get all users
-app.get("/all-users", async (req, res) => {
-  try {
-    const users = await User.find({});
-    res.status(200).json(users);
-  } catch (err) {
-    res.status(500).send({ message: "Something went wrong!" });
-  }
-});
-
-// Get a single user by its _id  ** POPULATE (populated thought and friend data) **
-app.get("/user/:id", async (req, res) => {
-  // console.log(req.params.id);
-  try {
-    const singleUser = await User.findOne({ _id: req.params.id });
-
-    res.status(200).json(singleUser);
-  } catch (err) {
-    res.status(500).send({ message: "Something went wrong!" });
-  }
-});
-
-// Create a new user
-app.post("/new-user", async (req, res) => {
-  const newUser = new User(req.body);
-  newUser.save();
-  if (newUser) {
-    res.status(200).json(newUser);
-  } else {
-    console.log("Something went wrong!");
-    res.status(500).json({ message: "Something went wrong!" });
-  }
-});
-
-// Delete a user by its _id **Remove a user's associated thoughts when deleted.**
-app.delete("/delete-user/:id", async (req, res) => {
-  try {
-    const deletedUser = await User.findOneAndDelete({ _id: req.params.id });
-    res.status(200).json(deletedUser);
-  } catch (err) {
-    res.status(500).send({ message: "Something went wrong!" });
-  }
-});
-
-// Update a user by its _id **Remove a user's associated thoughts when deleted.**
-app.put("/update-user/:id", async (req, res) => {
-  try {
-    const updatedUser = await User.findOneAndUpdate(
-      { _id: req.params.id },
-      req.body,
-      { new: true }
-    );
-    res.status(200).json(updatedUser);
-  } catch (err) {
-    res.status(500).send({ message: "Something went wrong!" });
-  }
-});
+app.use(routes);
 
 // Start the express server
 db.once("open", () => {
@@ -72,5 +15,3 @@ db.once("open", () => {
     console.log(`API server running on port ${PORT}!`);
   });
 });
-
-// VIDEO 1:25
